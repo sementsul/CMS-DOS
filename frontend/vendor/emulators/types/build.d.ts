47b348afa65a1539bbdd0f3aa649ac3223373e9c
Sementsul <45505876+sementsul@users.noreply.github.com>
2026-07-05 11:37:00 +0300

    gitdos: frontend/vendor/emulators/types/build.d.ts

---
+++ frontend/vendor/emulators/types/build.d.ts
@@ -0,0 +1,36 @@
+export declare const Build: {
+    version: string;
+    buildSeed: number;
+    "wdosbox-x-jspi.wasm": {
+        size: number;
+        gzSize: number;
+    };
+    "wdosbox-x-jspi.js": {
+        size: number;
+        gzSize: number;
+    };
+    "wdosbox-x.wasm": {
+        size: number;
+        gzSize: number;
+    };
+    "wdosbox-x.js": {
+        size: number;
+        gzSize: number;
+    };
+    "wdosbox.wasm": {
+        size: number;
+        gzSize: number;
+    };
+    "wdosbox.js": {
+        size: number;
+        gzSize: number;
+    };
+    "wlibzip.wasm": {
+        size: number;
+        gzSize: number;
+    };
+    "wlibzip.js": {
+        size: number;
+        gzSize: number;
+    };
+};
